7f9530b2f3ec5d7c1a7b177aa18e7390a73aaa81
Mohit-Doshi <mohit.dosi@gmail.com>
2021-04-16 21:07:03 -0700

    Pytorch NN Binary Classifier implementation

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import numpy as np
 from statistics import mean
 from statistics import median
 from sklearn import preprocessing
+from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.linear_model import LinearRegression, LogisticRegression
@@ -21,9 +22,15 @@ from sklearn.ensemble import RandomForestClassifier
 
 # one-class svm for imbalanced binary classification
 from sklearn.datasets import make_classification
-from sklearn.metrics import f1_score
+from sklearn.metrics import f1_score, confusion_matrix, classification_report
+
 from sklearn.svm import OneClassSVM
 
+# pytorch
+import torch
+import torch.nn as nn
+from torch.utils.data import Dataset, DataLoader
+import torch.optim as optim
 
 
 # df = pd.read_csv("PS_2020.10.26_11.22.58.csv")
@@ -428,3 +435,129 @@ preds = Pred_y.tolist()
 for vc in range(len(preds)):
     if preds[vc]  == -1:
         print(names_X[vc])
+
+
+# neural network here
+
+# Standardize input on split data
+scaler = StandardScaler()
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
+
+
+## train data
+class trainData(Dataset):
+
+    def __init__(self, X_data, y_data):
+        self.X_data = X_data
+        self.y_data = y_data
+
+    def __getitem__(self, index):
+        return self.X_data[index], self.y_data[index]
+
+    def __len__(self):
+        return len(self.X_data)
+
+
+train_data = trainData(torch.FloatTensor(X_train), torch.FloatTensor(y_train))
+
+## test data
+class testData(Dataset):
+
+    def __init__(self, X_data):
+        self.X_data = X_data
+
+    def __getitem__(self, index):
+        return self.X_data[index]
+
+    def __len__(self):
+        return len(self.X_data)
+
+
+test_data = testData(torch.FloatTensor(X_test))
+
+train_loader = DataLoader(dataset=train_data, batch_size=64, shuffle=True)  #FIXME: Set size
+test_loader = DataLoader(dataset=test_data, batch_size=1)
+
+# Neural Net Architecture
+class binaryClassification(nn.Module):
+    def __init__(self):
+        super(binaryClassification, self).__init__()
+        # Number of input features is 5.
+        self.layer_1 = nn.Linear(5, 64)
+        self.layer_2 = nn.Linear(64, 64)
+        self.layer_out = nn.Linear(64, 1)
+
+        self.relu = nn.ReLU()
+        self.dropout = nn.Dropout(p=0.1)
+        self.batchnorm1 = nn.BatchNorm1d(64)
+        self.batchnorm2 = nn.BatchNorm1d(64)
+
+    def forward(self, inputs):
+        resx = self.relu(self.layer_1(inputs))
+        resx = self.batchnorm1(resx)
+        resx = self.relu(self.layer_2(resx))
+        resx = self.batchnorm2(resx)
+        resx = self.dropout(resx)
+        resx = self.layer_out(resx)
+
+        return resx
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+print(device)
+
+model = binaryClassification()
+model.to(device)
+print(model)
+criterion = nn.BCEWithLogitsLoss()
+optimizer = optim.Adam(model.parameters(), lr=0.001)    #FIXME: Add learning rate
+
+# Training the model
+def binary_acc(y_pred, y_test):
+    y_pred_tag = torch.round(torch.sigmoid(y_pred))
+
+    correct_results_sum = (y_pred_tag == y_test).sum().float()
+    acc = correct_results_sum / y_test.shape[0]
+    acc = torch.round(acc * 100)
+
+    return acc
+
+
+EPOCHS = 50
+model.train()
+for e in range(1, EPOCHS + 1):
+    epoch_loss = 0
+    epoch_acc = 0
+    for X_batch, y_batch in train_loader:
+        X_batch, y_batch = X_batch.to(device), y_batch.to(device)
+        optimizer.zero_grad()
+
+        y_pred = model(X_batch)
+
+        loss = criterion(y_pred, y_batch.unsqueeze(1))
+        acc = binary_acc(y_pred, y_batch.unsqueeze(1))
+
+        loss.backward()
+        optimizer.step()
+
+        epoch_loss += loss.item()
+        epoch_acc += acc.item()
+
+    print(f'Epoch {e + 0:03}: | Loss: {epoch_loss / len(train_loader):.5f} | Acc: {epoch_acc / len(train_loader):.3f}')
+
+
+# Test the model
+y_pred_list = []
+model.eval()
+with torch.no_grad():
+    for X_batch in test_loader:
+        X_batch = X_batch.to(device)
+        y_test_pred = model(X_batch)
+        y_test_pred = torch.sigmoid(y_test_pred)
+        y_pred_tag = torch.round(y_test_pred)
+        y_pred_list.append(y_pred_tag.cpu().numpy())
+
+y_pred_list = [a.squeeze().tolist() for a in y_pred_list]
+
+
+print(classification_report(y_test, y_pred_list))
